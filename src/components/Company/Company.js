dbf9810d41f51d65a18296a175d0fbd997e63e5e
BedeMS <47581757+BedeMS@users.noreply.github.com>
2021-04-02 07:29:09 -0400

    Added Job card and job post component to Company component and styled it

--- src/components/Company/Company.js
+++ src/components/Company/Company.js
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "../Header/Header";
 import SearchBar from "../SearchBar/SearchBar";
 import CompanyDetails from "./CompanyDetails/CompanyDetails";
+import JobCard from "../JobCard/JobCard";
+import JobPost from "../JobPost/JobPost";
 import classes from "./Company.module.css";
 
 function Company(props) {
@@ -10,6 +12,29 @@ function Company(props) {
       <Header />
       <SearchBar companies />
       <CompanyDetails />
+      <h1 className={classes.Company__jobs__title}>Company Job Listings</h1>
+      <div className={classes.Company__jobs}>
+        <div className={classes.Company__joblistings}>
+          <JobCard />
+          <JobCard />
+          <JobCard />
+          <JobCard />
+          <JobCard />
+          <JobCard />
+          <JobCard />
+          <JobCard />
+          <JobCard />
+          <JobCard />
+          <JobCard />
+          <JobCard />
+          <JobCard />
+          <JobCard />
+          <JobCard />
+          <JobCard />
+          <JobCard />
+        </div>
+        <JobPost />
+      </div>
     </div>
   );
 }
